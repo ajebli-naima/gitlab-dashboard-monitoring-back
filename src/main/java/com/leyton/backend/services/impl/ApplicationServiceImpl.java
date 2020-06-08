5f611c5aec4d4fd5432a90256da0a55433607085
Faical CHABLOU <fchablou@leyton.com>
2020-06-08 18:28:07 +0100

    initial commit

---
+++ src/main/java/com/leyton/backend/services/impl/ApplicationServiceImpl.java
@@ -0,0 +1,52 @@
+package com.leyton.backend.services.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leyton.backend.dto.ApplicationDTO;
+import com.leyton.backend.entities.Application;
+import com.leyton.backend.mappers.ApplicationMapper;
+import com.leyton.backend.repositories.ApplicationRepository;
+import com.leyton.backend.services.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ApplicationServiceImpl implements ApplicationService {
+
+    @Autowired
+    ApplicationRepository applicationRepository;
+    @Autowired
+    ApplicationMapper applicationMapper;
+
+    @Override
+    public List<ApplicationDTO> findAllApplications() {
+        List<Application> applications = applicationRepository.findAll();
+
+        return applicationMapper.toApplicationDTOs(applications);
+    }
+
+    @Override
+    public Application findApplication(Long idApplication) {
+        return applicationRepository.findById(idApplication).get();
+    }
+
+    @Override
+    public Application saveAppplication(String app, MultipartFile file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        ApplicationDTO applicationDTO = mapper.readValue(app, ApplicationDTO.class);
+
+        if (file != null) {
+            applicationDTO.setPhoto(file.getBytes());
+        } else {
+            Application appOrgin = applicationRepository.findById(applicationDTO.getId()).get();
+            applicationDTO.setPhoto(appOrgin.getPhoto());
+        }
+
+        Application application = applicationMapper.toApplication(applicationDTO);
+        return applicationRepository.save(application);
+    }
+}
---
+++ src/main/java/com/leyton/backend/utils/Paths.java
@@ -0,0 +1,13 @@
+package com.leyton.backend.utils;
+
+public class Paths {
+
+    private Paths() {
+    }
+
+    /* ApplicationController */
+    public static final String APPLICATION = "/application";
+    public static final String FIND_ALL = "/findAll";
+
+
+}
